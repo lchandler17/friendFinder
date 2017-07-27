16371d0780adcbe7d9c2c1b9bc60997fc4c6b942
Fig Chandler <Fig@Laurens-MacBook-Air.local>
2017-07-27 14:35:16 -0400

    working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,34 +8,37 @@ module.exports = function(app) {
 
 	app.post("/api/friends", function(req, res) {
 
-		var newEntry = friendsData[friendsData.length-1];
-		var totalScore = "";
+		var userData = req.body;
+		friendsData.push(userData);
+
+		var totalScore = 1;
 		var bestMatch = 50;
 		var matchIndex = "";
 
-		for (var i = 0; i < friendsData.length; i++) {
+		// console.log("user:" + userData.name);
+		// console.log("Last index:" + friendsData[friendsData.length-1].name);
+
+		for (var i = 0; i < friendsData.length-1; i++) {
 			totalScore = 
-		    	Math.abs(parseInt(friendsData[i].scores[0]) - parseInt(newEntry.scores[0])) +
-		    	Math.abs(parseInt(friendsData[i].scores[1]) - parseInt(newEntry.scores[1])) +
-		    	Math.abs(parseInt(friendsData[i].scores[2]) - parseInt(newEntry.scores[2])) +
-		    	Math.abs(parseInt(friendsData[i].scores[3]) - parseInt(newEntry.scores[3])) +
-		    	Math.abs(parseInt(friendsData[i].scores[4]) - parseInt(newEntry.scores[4])) +
-		    	Math.abs(parseInt(friendsData[i].scores[5]) - parseInt(newEntry.scores[5])) +
-		    	Math.abs(parseInt(friendsData[i].scores[6]) - parseInt(newEntry.scores[6])) +
-		    	Math.abs(parseInt(friendsData[i].scores[7]) - parseInt(newEntry.scores[7])) +
-		    	Math.abs(parseInt(friendsData[i].scores[8]) - parseInt(newEntry.scores[8])) +
-		    	Math.abs(parseInt(friendsData[i].scores[9]) - parseInt(newEntry.scores[9])) +
-		    	Math.abs(parseInt(friendsData[i].scores[10]) - parseInt(newEntry.scores[10]));
+		    	Math.abs(parseInt(friendsData[i].scores[0]) - parseInt(userData.scores[0])) +
+		    	Math.abs(parseInt(friendsData[i].scores[1]) - parseInt(userData.scores[1])) +
+		    	Math.abs(parseInt(friendsData[i].scores[2]) - parseInt(userData.scores[2])) +
+		    	Math.abs(parseInt(friendsData[i].scores[3]) - parseInt(userData.scores[3])) +
+		    	Math.abs(parseInt(friendsData[i].scores[4]) - parseInt(userData.scores[4])) +
+		    	Math.abs(parseInt(friendsData[i].scores[5]) - parseInt(userData.scores[5])) +
+		    	Math.abs(parseInt(friendsData[i].scores[6]) - parseInt(userData.scores[6])) +
+		    	Math.abs(parseInt(friendsData[i].scores[7]) - parseInt(userData.scores[7])) +
+		    	Math.abs(parseInt(friendsData[i].scores[8]) - parseInt(userData.scores[8])) +
+		    	Math.abs(parseInt(friendsData[i].scores[9]) - parseInt(userData.scores[9]));
+		    // console.log(totalScore);
 
 		    if (totalScore < bestMatch) {
 		    	bestMatch = totalScore;
+		    	// console.log("bestmatch:" + bestMatch);
 		    	matchIndex = i;
+		    	// console.log("matchindex:" + matchIndex);
 		    }
-
-		    console.log(totalScore);
 		}
-
-		console.log(matchIndex);
 
 		res.json(friendsData[matchIndex]);
 
